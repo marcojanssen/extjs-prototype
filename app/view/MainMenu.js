f610c2f960c2cc96a331822a2f24b3e058a20d82
Marco Janssen <marco.janssen@outlook.com>
2013-07-29 21:35:02 +0200

    Fixed navigation

--- app/view/MainMenu.js
+++ app/view/MainMenu.js
@@ -11,14 +11,14 @@ Ext.define('Prototype.view.MainMenu', {
     html: '<div class="container pull-left">' +
               '<ul class="menu">' +
                   '<li><a href="#">Product</a></li>' +
-                  '<li class="page1"><a href="#home">Page 1</a></li>' +
-                  '<li class="page2"><a href="#home">Page 2</a></li>' +
+                  '<li class="page1"><a href="#">Page 1</a></li>' +
+                  '<li class="page2"><a href="#settings">Page 2</a></li>' +
               '</ul>' +
           '</div>' +
           '<div class="container pull-right">' +
              '<ul class="menu">' +
-                '<li class="home"><a href="#home">Home</a></li>' +
-                '<li class="profile"><a href="#home">Marco Janssen</a></li>' +
+                '<li class="home"><a href="#">Home</a></li>' +
+                '<li class="profile"><a href="#">Marco Janssen</a></li>' +
             '</ul>' +
           '</div>'
 });
