762652454c88cce46bc134351e4c26df9a29005e
Marco Janssen <marco.janssen@outlook.com>
2013-07-29 13:28:55 +0200

    Added class to container menu

--- app/view/MainMenu.js
+++ app/view/MainMenu.js
@@ -2,18 +2,19 @@ Ext.define('Prototype.view.MainMenu', {
     extend: 'Ext.container.Container',
 
     xtype: 'MainMenu',
-    height: 50,
+    height: 35,
 
     bodyBorder: false,
     border: false,
+    cls: 'menu',
 
-    html: '<div class="container menu pull-left">' +
+    html: '<div class="container pull-left">' +
               '<ul class="menu">' +
                   '<li><a href="#home">a</a></li>' +
                   '<li><a href="#home">b</a></li>' +
               '</ul>' +
           '</div>' +
-          '<div class="container menu pull-right">' +
+          '<div class="container pull-right">' +
              '<ul class="menu">' +
                 '<li><a href="#home">a</a></li>' +
                 '<li><a href="#home">b</a></li>' +
