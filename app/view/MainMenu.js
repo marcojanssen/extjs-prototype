c3b51c09c463d69b5ed1f64f8e9050569b25b939
Marco Janssen <marco.janssen@outlook.com>
2013-07-29 17:07:37 +0200

    Added icons and dummy text for main menu

--- app/view/MainMenu.js
+++ app/view/MainMenu.js
@@ -10,14 +10,15 @@ Ext.define('Prototype.view.MainMenu', {
 
     html: '<div class="container pull-left">' +
               '<ul class="menu">' +
-                  '<li><a href="#home">a</a></li>' +
-                  '<li><a href="#home">b</a></li>' +
+                  '<li><a href="#">Product</a></li>' +
+                  '<li class="page1"><a href="#home">Page 1</a></li>' +
+                  '<li class="page2"><a href="#home">Page 2</a></li>' +
               '</ul>' +
           '</div>' +
           '<div class="container pull-right">' +
              '<ul class="menu">' +
-                '<li><a href="#home">a</a></li>' +
-                '<li><a href="#home">b</a></li>' +
+                '<li class="home"><a href="#home">Home</a></li>' +
+                '<li class="profile"><a href="#home">Marco Janssen</a></li>' +
             '</ul>' +
           '</div>'
 });
