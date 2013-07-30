97c6b383b282b37f0f9a7a633d2d2d929dd17500
Marco Janssen <marco.janssen@outlook.com>
2013-07-30 22:40:48 +0200

    Submenu items are variable depending on the controller used

--- app/controller/SubMenu.js
+++ app/controller/SubMenu.js
@@ -7,23 +7,88 @@ Ext.define('Prototype.controller.SubMenu', {
     }],
 
     init: function() {
-        Ext.ux.Router.on('beforedispatch', this.hideSubMenu, this);
-        Ext.ux.Router.on('dispatch', this.initSubMenu, this);
+        Ext.ux.Router.on('beforedispatch', this.beforeDispatchSubMenu, this);
+        Ext.ux.Router.on('dispatch', this.dispatchSubMenu, this);
     },
 
-    hideSubMenu: function(uri, match) {
+    /**
+     * Slide the submenu out of view
+     * @param uri
+     * @param match
+     */
+    beforeDispatchSubMenu: function(uri, match) {
+        this.slideOut();
+    },
+
+    /**
+     * Add the buttons and slide in if there are any buttons
+     * @param uri
+     * @param match
+     * @param params
+     * @param controller
+     */
+    dispatchSubMenu: function(uri, match, params, controller) {
         var submenu = this.getSubMenu();
-        submenu.getEl().slideOut('t', {
-            duration: 400,
-            scope: this
-        });
+
+        submenu.removeAll();
+
+        if('Settings' === match.controller) {
+            submenu.add(this.getSettingsSubMenuItems());
+        }
+
+        if('Home' === match.controller) {
+            submenu.add(this.getHomeSubMenuItems());
+        }
+
+        if(null !== submenu.child()) {
+            this.slideIn(submenu.getEl());
+        }
     },
 
-    initSubMenu: function(uri, match) {
+    /**
+     * Get the items for settings
+     * @returns {Array}
+     */
+    getSettingsSubMenuItems: function() {
+        return [];
+    },
+
+    /**
+     * Get the items for home
+     * @returns {Array}
+     */
+    getHomeSubMenuItems: function() {
+        return [{
+            text: 'New'
+        }];
+    },
+
+    /**
+     * Slide in the submenu
+     * @param subMenuElement
+     */
+    slideIn: function() {
         var submenu = this.getSubMenu();
         submenu.getEl().slideIn('t', {
             duration: 400,
+            callback: function() {
+                submenu.doLayout();
+            },
             scope: this
         });
+
+    },
+
+    /**
+     * Slide out the submenu
+     * @param subMenuElement
+     */
+    slideOut: function() {
+        var submenu = this.getSubMenu();
+        submenu.getEl().slideOut('t', {
+            duration: 400
+        });
     }
+
+
 });
