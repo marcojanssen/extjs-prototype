fcccd207ccd6da14a7ec07afe04f9bcce674f2fb
Marco Janssen <marco.janssen@outlook.com>
2013-07-30 23:28:15 +0200

    Added splashscreen loader

--- app.js
+++ app.js
@@ -14,3 +14,5 @@ Ext.application({
     
     autoCreateViewport: true
 });
+
+
--- app/application.js
+++ app/application.js
@@ -1,3 +1,15 @@
+Ext.onReady(function() {
+    // Start the mask on the body and get a reference to the mask
+    splashscreen = Ext.getBody().mask('Loading application', 'splashscreen');
+    // Add a new class to this mask as we want it to look different from the default.
+    splashscreen.addCls('splashscreen');
+
+    // Insert a new div before the loading icon where we can place our logo.
+    Ext.DomHelper.insertFirst(Ext.query('.x-mask-msg')[0], {
+        cls: 'x-splash-icon'
+    });
+});
+
 Ext.define('Prototype.Application', {
     name: 'Prototype',
 
@@ -42,6 +54,35 @@ Ext.define('Prototype.Application', {
 
     launch: function() {
 
+        this.fadeSplash();
+        this.initRouter();
+    },
+
+    fadeSplash: function() {
+        // Setup a task to fadeOut the splashscreen
+        var task = new Ext.util.DelayedTask(function() {
+            // Fade out the body mask
+            splashscreen.fadeOut({
+                duration: 1000,
+                remove:true
+            });
+            // Fade out the icon and message
+            splashscreen.next().fadeOut({
+                duration: 1000,
+                remove:true,
+                listeners: {
+                    afteranimate: function() {
+                        // Set the body as unmasked after the animation
+                        Ext.getBody().unmask();
+                    }
+                }
+            });
+        });
+        // Run the fade 500 milliseconds after launch.
+        task.delay(500);
+    },
+
+    initRouter: function() {
         /*
          * Ext.ux.Router provides some events for better controlling
          * dispatch flow
